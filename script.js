f2feef89e963850c9eb2b2aeedc3ab943fce4747
fandyputram <clonzf@gmail.com>
2018-03-11 23:36:00 +0700

    Randomize 1.0

---
+++ script.js
@@ -0,0 +1,33 @@
+function generateRandomNumber() {
+	document.getElementById("result").innerHTML = null;
+
+	var min = Number(document.randomData.min.value);
+	var max = Number(document.randomData.max.value);
+	var amount = Number(document.randomData.amount.value);
+
+	var diff=max-min+1;
+
+	if(diff<amount)
+	{
+		document.getElementById("alert").innerHTML="Please input an appropriate value!"
+	}
+	else
+	{
+		document.getElementById("alert").innerHTML=null;
+		var status=[false];
+		for(i = 0;i < amount; i++)
+		{
+			var num=(Math.floor(Math.random() * diff) + min);
+			if(status[num]!=true)
+			{
+				var randomNumber = '<div>' + num + '</div>';
+				status[num]=true;
+				document.getElementById("result").innerHTML += randomNumber;
+			}
+			else
+			{
+				i--;
+			}
+		}
+	}
+}
